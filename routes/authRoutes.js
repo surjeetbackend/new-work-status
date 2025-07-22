5abc748e4aab69da45eb6367c29f1639ab9905d7
surjeetbackend <backendoffice12@gmail.com>
2025-07-22 15:42:57 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
-// const { registerUser, loginUser } = require('../controllers/authController');
-const {registerSendOTP,registerVerifyOTP,loginSendOTP,loginVerifyOTP } = require('../controllers/authController');
-
+const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
+const {
+  registerSendOTP,
+  registerVerifyOTP,
+  loginSendOTP,
+  loginVerifyOTP,
+  getProfile
+} = require('../controllers/authController');
 
 router.post('/register/send-otp', registerSendOTP);
 router.post('/register/verify-otp', registerVerifyOTP);
-
-
 router.post('/login/send-otp', loginSendOTP);
 router.post('/login/verify-otp', loginVerifyOTP);
 
-// router.post('/register', registerUser);
-// router.post('/login', loginUser);
+// ✅ Protect the profile route
+router.get('/profile', verifyToken, getProfile);
 
 module.exports = router;
