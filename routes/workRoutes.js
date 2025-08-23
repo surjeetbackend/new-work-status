30cd738e40fb1423bb6d66ab64a0cdcda832fc84
surjeetbackend <backendoffice12@gmail.com>
2025-08-23 18:20:23 +0530

    Update workRoutes.js

--- routes/workRoutes.js
+++ routes/workRoutes.js
@@ -1,7 +1,7 @@
 // routes/workRoutes.js
 const express = require('express');
 const router = express.Router();
-const { createWork } = require('../controllers/workController');
+const { createWork,clientFeedback } = require('../controllers/workController');
 const { verifyToken, authorizeRoles } = require('../middleware/authMiddleware');
 const Work = require('../models/Work'); 
 
@@ -12,13 +12,13 @@ router.post('/submit', verifyToken, authorizeRoles('client'), createWork);
 router.get('/my-works', verifyToken, authorizeRoles('client'), async (req, res) => {
   try {
     const works = await Work.find({ client_id: req.user._id })
-      .populate('assigned_to', 'name')
+      .populate('assigned_to', 'name email phone')
       .populate('approvalBy', 'name'); 
     res.json(works);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
-
+router.put('/client-feedback/:id',verifyToken,authorizeRoles('client'),clientFeedback);
 
 module.exports = router;
